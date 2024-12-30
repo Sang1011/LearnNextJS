6d6b0b826560dca95b5b80a9d14a8aefc81a8dd5
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-12-30 16:16:01 +0700

    fix bug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true, // Chế độ Strict Mode để phát hiện lỗi
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Rewrites cho API routes
+        destination: "/api/:path*",
+      },
+      {
+        source: "/:path*", // Rewrites đảm bảo route chính hoạt động
+        destination: "/", // Điều hướng tới trang chính
+      },
+    ];
+  },
 };
 
 export default nextConfig;
